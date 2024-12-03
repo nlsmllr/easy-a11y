5b327c83f0c1130ad3b943fab1686cc947dc1865
NMUProtofy <nils.mueller@protofy.com>
2024-12-03 11:28:11 +0100

    test github actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid min-h-screen cursor-none items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
+    <div className="grid min-h-screen items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
       <main className="row-start-2 flex flex-col items-center justify-center gap-8">
         <p className="text-8xl font-black uppercase tracking-wide">Easy a11y</p>
       </main>
