9d4514ffc7531e4eef152e7e68a77193bd2e4182
NMUProtofy <nils.mueller@protofy.com>
2024-12-03 11:23:40 +0100

    project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="grid min-h-screen cursor-none items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
+      <main className="row-start-2 flex flex-col items-center justify-center gap-8">
+        <p className="text-8xl font-black uppercase tracking-wide">Easy a11y</p>
+      </main>
+      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6"></footer>
+    </div>
+  );
+}
