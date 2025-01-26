b997d0ca66f485761ad394a288615456df4daf91
NMUProtofy <nils.mueller@protofy.com>
2025-01-26 16:37:50 +0100

    13th attempt to display detailed a11y report in PR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="container mx-auto p-4">
       <h1 className="mb-4 text-2xl font-bold text-gray-100">URL Checker</h1>
       <URLCheck />
+      <img src="image_01.jpeg" alt="" />
     </main>
   );
 }
