b9b5d142a31eef88ca7933849c1d111b09e1ac07
NMUProtofy <nils.mueller@protofy.com>
2025-01-29 14:46:32 +0100

    fix: only a11y details are printed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import URLCheck from './components/URLCheck';
 export default function Home() {
   return (
     <main className="container mx-auto p-4">
-      <h1 className="mb-4 text-2xl font-bold text-gray-900">URL Checker</h1>
+      <h1 className="mb-4 text-2xl font-bold text-gray-100">URL Checker</h1>
       <URLCheck />
       <img src="image_01.jpeg" alt="imagess" height={500} width={500} />
     </main>
