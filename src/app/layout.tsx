5b45a3071b2fae80f4e6c5b3c3b024a9c97b7340
NMUProtofy <nils.mueller@protofy.com>
2025-01-30 17:47:07 +0100

    fix: html validator - Lighthouse is turned off

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" property="test property" content="test content">
+    <html lang="en">
+      <meta property="test property" content="test content" />
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
