6555b1df0282a5238403d40171fc3c2ddab37fd2
NMUProtofy <nils.mueller@protofy.com>
2025-01-30 17:09:31 +0100

    fix: html validator - Lighthouse is turned off

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,13 +26,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Easy access to accessibility resources and guidelines" />
-        <meta name="keywords" content="accessibility, a11y, web design, inclusive design" />
-        <meta name="author" content="Your Name or Your Organization's Name" />
-      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
