4a80c6ff5c82f6cbcc57a1aab12fe07091d8cf4d
NMUProtofy <nils.mueller@protofy.com>
2025-02-03 08:54:15 +0100

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import URLCheck from './components/URLCheck';
 export default function Home() {
   return (
     <main className="container mx-auto p-4">
-      <h1 className="mb-4 text-2xl font-bold text-gray-100">URL Checker</h1>
+      <h1 className="mb-4 text-2xl font-bold text-gray-900">URL Checker</h1>
       <URLCheck />
-      <img src="image_01.jpeg" alt="photooo" height={500} width={500} />
+      <img src="image_01.jpeg" alt="photos" height={500} width={500} />
     </main>
   );
 }
