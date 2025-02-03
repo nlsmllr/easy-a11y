e8f967030a1d2fff61404e1c9d8a5072b259b138
NMUProtofy <nils.mueller@protofy.com>
2025-02-03 16:00:02 +0100

    add: lighthouse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import URLCheck from './components/URLCheck';
 
 export default function Home() {
@@ -5,6 +7,7 @@ export default function Home() {
     <main className="container mx-auto p-4">
       <h1 className="mb-4 text-2xl font-bold text-gray-900">URL Checker</h1>
       <URLCheck />
+      <Image src="image_01.jpeg" alt="photo" height={500} width={500}></Image>
       <img src="image_01.jpeg" alt="photos" height={500} width={500} />
     </main>
   );
