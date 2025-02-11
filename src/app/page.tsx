573ad5b1a3ae4d6d0ad119a182c5e4d9ab5d41d9
NMUProtofy <nils.mueller@protofy.com>
2025-02-11 14:24:15 +0100

    add: new structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="container mx-auto p-4">
       <h1 className="mb-4 text-2xl font-bold text-gray-900">URL Checker</h1>
       <URLCheck />
-      <Image src="image_01.jpeg" alt="example photo" height={500} width={500}></Image>
+      <Image src="image_01.jpeg" alt="abstract waves" height={500} width={500}></Image>
       {/* <img src="image_01.jpeg" alt="" height={500} width={500} /> */}
     </main>
   );
