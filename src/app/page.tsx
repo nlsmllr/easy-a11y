ad017200f94243a151def0c2734a9c9402b061c9
NMUProtofy <nils.mueller@protofy.com>
2025-02-19 11:11:40 +0100

    fix: minor changes in lighthouse wf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import URLCheck from './components/URLCheck';
 export default function Home() {
   return (
     <main className="container mx-auto p-4">
-      <h1 className="mb-4 text-2xl font-bold text-gray-900">URL Checker</h1>
+      <h1 className="mb-4 text-2xl font-bold text-gray-100">URL Checker</h1>
       <URLCheck />
       <Image src="image_01.jpeg" alt="abstract waves" height={250} width={250} />
       {/* <Image src="image_01.jpeg" height={250} width={250} /> */}
