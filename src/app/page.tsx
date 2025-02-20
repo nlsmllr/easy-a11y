b02786fc63b23d6d9e95d9f06fb2ca35ff520b8d
NMUProtofy <nils.mueller@protofy.com>
2025-02-20 07:56:46 +0100

    fix: minor changes page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
       <h1 className="mb-4 text-2xl font-bold text-gray-900">URL Checker</h1>
       <URLCheck />
       <Image src="image_01.jpeg" alt="abstract waves" height={500} width={500} />
+
       {/* The following lines are only examples to test the ESLint plugin */}
       {/* <Image src="image_01.jpeg" height={250} width={250} /> */}
       {/* <Image src="image_01.jpeg" alt="" height={250} width={250} /> */}
